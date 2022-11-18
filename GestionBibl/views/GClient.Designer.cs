948f57c4881145c922c5799337ed8f057e2c630b
BENATTIK Amal <amalbenattik7@gmail.com>
2022-11-18 19:07:47 +0100

    gestion client v1

--- GestionBibl/views/GClient.Designer.cs
+++ GestionBibl/views/GClient.Designer.cs
@@ -28,18 +28,203 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtNom = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPrenom = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCIN = new System.Windows.Forms.TextBox();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.btnAjouter = new System.Windows.Forms.Button();
+            this.btnModifier = new System.Windows.Forms.Button();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnSupprimer = new System.Windows.Forms.Button();
+            this.txtSrch = new System.Windows.Forms.TextBox();
+            this.btnSrch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.SuspendLayout();
+            // 
+            // txtNom
+            // 
+            this.txtNom.Location = new System.Drawing.Point(35, 54);
+            this.txtNom.Name = "txtNom";
+            this.txtNom.Size = new System.Drawing.Size(156, 23);
+            this.txtNom.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(91, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nom";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(269, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Prenom";
+            // 
+            // txtPrenom
+            // 
+            this.txtPrenom.Location = new System.Drawing.Point(212, 54);
+            this.txtPrenom.Name = "txtPrenom";
+            this.txtPrenom.Size = new System.Drawing.Size(156, 23);
+            this.txtPrenom.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(453, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "CIN";
+            // 
+            // txtCIN
+            // 
+            this.txtCIN.Location = new System.Drawing.Point(392, 54);
+            this.txtCIN.Name = "txtCIN";
+            this.txtCIN.Size = new System.Drawing.Size(156, 23);
+            this.txtCIN.TabIndex = 4;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(23, 171);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowTemplate.Height = 25;
+            this.dataGridView.Size = new System.Drawing.Size(708, 150);
+            this.dataGridView.TabIndex = 6;
+            this.dataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellContentClick);
+            this.dataGridView.SelectionChanged += new System.EventHandler(this.dataGridView_SelectionChanged);
+            // 
+            // btnAjouter
+            // 
+            this.btnAjouter.Location = new System.Drawing.Point(270, 83);
+            this.btnAjouter.Name = "btnAjouter";
+            this.btnAjouter.Size = new System.Drawing.Size(75, 23);
+            this.btnAjouter.TabIndex = 7;
+            this.btnAjouter.Text = "Ajouter";
+            this.btnAjouter.UseVisualStyleBackColor = true;
+            this.btnAjouter.Click += new System.EventHandler(this.btnAjouter_Click);
+            // 
+            // btnModifier
+            // 
+            this.btnModifier.Location = new System.Drawing.Point(351, 83);
+            this.btnModifier.Name = "btnModifier";
+            this.btnModifier.Size = new System.Drawing.Size(75, 23);
+            this.btnModifier.TabIndex = 8;
+            this.btnModifier.Text = "Modifier";
+            this.btnModifier.UseVisualStyleBackColor = true;
+            this.btnModifier.Click += new System.EventHandler(this.btnModifier_Click);
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(65, 3);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(156, 23);
+            this.txtId.TabIndex = 9;
+            this.txtId.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(636, 36);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 15);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Statut";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Bloquer",
+            "Debloquer"});
+            this.comboBox1.Location = new System.Drawing.Point(575, 54);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(156, 23);
+            this.comboBox1.TabIndex = 12;
+            // 
+            // btnSupprimer
+            // 
+            this.btnSupprimer.Location = new System.Drawing.Point(432, 83);
+            this.btnSupprimer.Name = "btnSupprimer";
+            this.btnSupprimer.Size = new System.Drawing.Size(75, 23);
+            this.btnSupprimer.TabIndex = 13;
+            this.btnSupprimer.Text = "Supprimer";
+            this.btnSupprimer.UseVisualStyleBackColor = true;
+            this.btnSupprimer.Click += new System.EventHandler(this.btnSupprimer_Click);
+            // 
+            // txtSrch
+            // 
+            this.txtSrch.Location = new System.Drawing.Point(494, 143);
+            this.txtSrch.Name = "txtSrch";
+            this.txtSrch.Size = new System.Drawing.Size(156, 23);
+            this.txtSrch.TabIndex = 14;
+            this.txtSrch.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnSrch
+            // 
+            this.btnSrch.Location = new System.Drawing.Point(656, 142);
+            this.btnSrch.Name = "btnSrch";
+            this.btnSrch.Size = new System.Drawing.Size(75, 23);
+            this.btnSrch.TabIndex = 15;
+            this.btnSrch.Text = "Rechercher";
+            this.btnSrch.UseVisualStyleBackColor = true;
+            this.btnSrch.Click += new System.EventHandler(this.button1_Click);
             // 
             // GClient
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnSrch);
+            this.Controls.Add(this.txtSrch);
+            this.Controls.Add(this.btnSupprimer);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.btnModifier);
+            this.Controls.Add(this.btnAjouter);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCIN);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPrenom);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNom);
             this.Name = "GClient";
             this.Size = new System.Drawing.Size(769, 376);
+            this.Load += new System.EventHandler(this.GClient_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private TextBox txtNom;
+        private Label label1;
+        private Label label2;
+        private TextBox txtPrenom;
+        private Label label3;
+        private TextBox txtCIN;
+        private DataGridView dataGridView;
+        private Button btnAjouter;
+        private Button btnModifier;
+        private TextBox txtId;
+        private Label label4;
+        private ComboBox comboBox1;
+        private Button btnSupprimer;
+        private TextBox txtSrch;
+        private Button btnSrch;
     }
 }
